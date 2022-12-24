6d5bb3d42c0f26587d69da382e4121140eaf8cf1
abhishandy <abhishek.sandy@gmail.com>
2022-12-23 21:02:17 -0500

    init react app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # baddle
+
 Pay-per use cloud storage on AWS S3
+
+[Notion note](https://abhishandy.notion.site/Baddle-Project-f8c635cbda28405aa806257c1165c1ff)
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+export const App = () => {
+  return (
+    <>
+      <div className="text-4xl text-center mt-4">Baddle!</div>
+      <div className="text-lg text-center mt-4">
+        Pay-per use cloud storage on AWS S3
+      </div>
+      <div className="text-lg text-center mt-4">
+        <a
+          href="https://github.com/abhiShandy/baddle"
+          target="_blank"
+          className="text-blue-800 underline"
+        >
+          GitHub
+        </a>
+      </div>
+    </>
+  );
+};
